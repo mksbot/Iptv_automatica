312ce92e25a399096d067b0975c66b144e7e7735
mksbot <81396341+mksbot@users.noreply.github.com>
2021-03-29 21:50:12 -0300

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,238 @@
+const help = (prefix) => {
+	return `
+<══════════════════════>
+      *RPG-ONE PIECE⚔️*
+ <══════════════════════>
+
+__ ● ᏴϴͲ●ᎷᎬΝႮ●        *DONO* : RPG-ONE.PIECE⚔️ 
+ᏴϴͲ Ꮩ4.8
+
+┌─────────❶.❷────────
+⚔️〘 INFORMAÇÕES 〙
+⚔️
+╠🐉 * RPG-ONE PIECE⚔️BOT* 
+╠🐉 𝐃𝐎𝐍𝐎:  ⃬⃗one piece (base dark) 
+╠🐉 *wa.me/+559885894652*
+╠🐉 𝐒𝐓𝐀𝐓𝐔𝐒: ON
+⚔️
+⚔️
+╠══🌀〘 MENU 〙🌀══
+⚔️
+⚔️🔀 *${prefix}figura*
+⚔️🔀 *${prefix}toimg*
+⚔️🔀 *${prefix}darkjokes (memes aleatórios)*
+⚔️🔀 *${prefix}memeindo*
+⚔️🔀 *${prefix}tts*
+⚔️🔀 *${prefix}lolih [on]*
+⚔️🔀 *${prefix}+18loli [off]*
+⚔️🔀 *${prefix}url2img*
+⚔️🔀 *${prefix}leens [na legenda]*
+⚔️🔀 *${prefix}wait [na legenda]*
+⚔️🔀 *${prefix}setprefix*
+⚔️
+╠══🌀〘 OUTROS 〙🌀══
+⚔️
+⚔️🔀 *${prefix}phlogo*
+⚔️🔀 *${prefix}galaxtext*
+⚔️🔀 *${prefix}linkgp*
+⚔️🔀 *${prefix}simih [1/0]*
+⚔️🔀 *${prefix}marcar*
+⚔️🔀 *${prefix}add [@]*
+⚔️🔀 *${prefix}banir [@]*
+⚔️🔀 *${prefix}promover [@]*
+⚔️🔀 *${prefix}rebaixar*
+⚔️🔀 *${prefix}admins*
+⚔️🔀 *${prefix}marcar2*
+⚔️🔀 *${prefix}bc [texto]* (ele faz uma ™)
+⚔️🔀 *${prefix}marcar3*
+⚔️🔀 *${prefix}bloqueados*
+⚔️🔀 *${prefix}bloquear [@]*
+⚔️🔀 *${prefix}desbloquear [@]*
+⚔️🔀 *${prefix}limpar*
+⚔️🔀 *${prefix}bala*
+⚔️🔀 *${prefix}bemvindo [1/0]*
+⚔️🔀 *${prefix}clonar [@]*
+⚔️🔀 *${prefix}help1*
+⚔️🔀 *${prefix}dono*
+⚔️🔀 *${prefix}owner*
+⚔️🔀 *${prefix}tts [texto]*
+⚔️🔀 *${prefix}setnome*
+⚔️🔀 *${prefix}termux*
+⚔️🔀 *${prefix}setfoto*
+⚔️🔀 *${prefix}grupoinfo*
+⚔️🔀 *${prefix}ytmp4*
+⚔️🔀 *${prefix}bomdia*
+⚔️🔀 *${prefix}boanoite*
+⚔️🔀 *${prefix}marcar*
+⚔️🔀 *${prefix}marcar2*
+⚔️🔀 *${prefix}marcar3*
+⚔️
+╠══🌀〘 IMAGENS 〙🌀══
+⚔️
+⚔️🔀 *${prefix}loli* [off]
+⚔️🔀 *${prefix}loli1*
+⚔️🔀 *${prefix}hentai*
+⚔️🔀 *${prefix}dono*
+⚔️🔀 *${prefix}porno*
+⚔️🔀 *${prefix}boanoite*
+⚔️🔀 *${prefix}bomdia*
+⚔️🔀 *${prefix}boatarde*
+⚔️🔀 *${prefix}mia [aleatórias]*
+⚔️🔀 *${prefix}rize [aleatórias]*
+⚔️🔀 *${prefix}minato [aleatórias]*
+⚔️🔀 *${prefix}boruto [aleatórias]*
+⚔️🔀 *${prefix}hinata [aleatórias]*
+⚔️🔀 *${prefix}sasuke [aleatórias]*
+⚔️🔀 *${prefix}sakura [aleatórias]*
+⚔️🔀 *${prefix}naruto [aleatórias]*
+⚔️🔀 *${prefix}meme*   
+⚔️🔀 *${prefix}lofi*
+⚔️🔀 *${prefix}malkova*
+⚔️🔀 *${prefix}canal*
+⚔️🔀 *${prefix}+18loli1*
+⚔️🔀 *${prefix}reislin*
+⚔️
+╠══🌀〘 INTELIGÊNCIA 〙🌀══
+⚔️
+⚔️🔀 *${prefix}simih 1 (para ativar)*
+⚔️🔀 *${prefix}simih 0 (para desativar)*
+⚔️ *${prefix}simi (sua mensagem)*
+⚔️
+╠══🌀〘 EM PRODUÇÃO 〙🌀══
+⚔️
+⚔️🔀 *${prefix}gado*
+⚔️🔀 *${prefix}dbz*
+⚔️🔀 *${prefix}gtts loli*
+⚔️🔀 *${prefix}hidegp*
+⚔️🔀 *${prefix}grupos
+⚔️🔀 *${prefix}antinazismo
+⚔️🔀 *${prefix}it 1/0
+⚔️
+╠══🌀〘 SÓ PREMIUM 〙🌀══
+⚔️
+⚔️🔀 *${prefix}dado*
+⚔️🔀 *${prefix}cekvip*
+⚔️🔀 *${prefix}premiumlist*
+⚔️🔀 *${prefix}delete*
+⚔️🔀 *${prefix}modapk*
+⚔️🔀 *${prefix}indo10*
+⚔️🔀 *${prefix}daftarvip [para virar Premium]*
+⚔️🔀 *${prefix}qrcode*
+⚔️🔀 *${prefix}chentai*
+⚔️🔀 *${prefix}gcpf*
+⚔️🔀 *${prefix}gbin*
+⚔️🔀 *${prefix}pack*
+⚔️🔀 *${prefix}destrava*
+⚔️🔀 *${prefix}gpessoa*
+⚔️
+╠══🌀〘 GRUPO 〙🌀══
+⚔️
+⚔️🔀 *${prefix}banir*
+⚔️🔀 *${prefix}leveling [on/off]*
+⚔️🔀 *${prefix}level*
+⚔️🔀 *${prefix}add*
+⚔️🔀 *${prefix}promover*
+⚔️🔀 *${prefix}setfoto [na legenda]*
+⚔️🔀 *${prefix}setname [texto]*
+⚔️🔀 *${prefix}rebaixar*
+⚔️🔀 *${prefix}admins*
+⚔️🔀 *${prefix}marcar*
+⚔️🔀 *${prefix}marcar2*
+⚔️🔀 *${prefix}marcar3*
+⚔️🔀 *${prefix}bemvindo [1/0]*
+⚔️🔀 *${prefix}grupoinfo*
+⚔️🔀 *${prefix}bomdia*
+⚔️🔀 *${prefix}boatarde*
+⚔️🔀 *${prefix}boanoite*
+⚔️🔀 *${prefix}setdesc*
+⚔️🔀 *${prefix}bug [sua mensagem]*
+⚔️
+╠══🌀〘 ESPECIFICO DO BOT 〙🌀══
+⚔️
+⚔️🔀 *${prefix}bug [sua mensagem]*
+⚔️🔀 *${prefix}clonar [@]*
+⚔️🔀 *${prefix}dono*
+⚔️🔀 *${prefix}ping [ver velocidade do bot]*
+⚔️🔀 *${prefix}addshit*
+⚔️🔀 *${prefix}delshit*
+⚔️🔀 *${prefix}gay [@]*
+⚔️🔀 *${prefix}wame*
+⚔️🔀 *${prefix}map (nome)*
+⚔️🔀 *${prefix}setppbot (marque uma img)*
+⚔️🔀 *${prefix}pinterest (nome)*
+⚔️🔀 *${prefix}desligar (so para o dono)*
+⚔️🔀 *${prefix}timer*
+⚔️
+╠══🌀〘 MAIS ALGUNS 〙🌀══
+⚔️
+⚔️🔀 *${prefix}neko*
+⚔️🔀 *${prefix}ttp [texto]*
+⚔️🔀 *${prefix}testime*
+⚔️🔀 *${prefix}tomp3*
+⚔️🔀 *${prefix}modoanime [on/off]*
+⚔️🔀 *${prefix}modo+18 [on/off]*
+⚔️🔀 *${prefix}happymod [jogo/app]*
+⚔️🔀 *${prefix}rize*
+⚔️🔀 *${prefix}ytsearch*
+⚔️🔀 *${prefix}moddroid [jogo/app]*
+⚔️🔀 *${prefix}xvideos [titulo]**
+⚔️🔀 *${prefix}nomegp*
+⚔️🔀 *${prefix}darkjokes (memes aleatórios)*
+⚔️🔀 *${prefix}animecry*
+⚔️🔀 *${prefix}gay1*
+⚔️🔀 *${prefix}next*
+⚔️🔀 *${prefix}alerta*
+⚔️🔀 *${prefix}belle [img aleatórias]*
+⚔️🔀 *${prefix}pronomeneu [texto]*
+⚔️🔀 *${prefix}hobby*
+⚔️🔀 *${prefix}kiss
+⚔️🔀 *${prefix}analise
+⚔️
+╠══🌀〘 COMANDOS DE VOZ 〙🌀══
+⚔️
+⚔️🔀 *${prefix}ola*
+⚔️🔀 *${prefix}bv*
+⚔️🔀 *${prefix}tchau*
+⚔️🔀 *${prefix}bem*
+⚔️🔀 *${prefix}a*
+⚔️🔀 *${prefix}fdp*
+⚔️🔀 *${prefix}onich*
+⚔️🔀 *${prefix}beat1*
+⚔️🔀 *${prefix}glub*
+⚔️🔀 *${prefix}MIKASU*
+⚔️
+╠══🌀〘 OUTROS /2 〙🌀══
+⚔️
+⚔️🔀 *${prefix}antilink [1/0]*
+⚔️🔀 *${prefix}brainly [pergunta]*
+⚔️🔀 *${prefix}antiracismo [on/off]*
+⚔️🔀 *${prefix}setnomebot*
+⚔️🔀 *${prefix}meme*
+⚔️
+╠══🌀〘 INTERATIVOS 〙🌀══
+⚔️
+╠══NOTA »
+⚔️Mandar a msg sem o prefixo
+╠════════════════════
+⚔️😝 *nudis*
+⚔️😝 *bom dia bot*
+⚔️😝 *boa tarde bot*
+⚔️😝 *boa noite bot*
+⚔️😝 *grupo*
+⚔️😝 *grita*
+⚔️😝 *MIKASU*
+⚔️😝 *videos adultos*
+⚔️😝 *lindo*
+⚔️
+╠══🌀〘 MIKASU NO CONTROLE 〙🌀══
+⚔️
+⚔️ *NOME: MIKASU*
+⚔️ *INSTA: *
+⚔️ *WPP: wa.me/+5528981227647*
+⚔️ *GRUPO: https://chat.whatsapp.com/BtRS40MeoLuGJCIJdeE8XD
+⚔️
+╚═〘 RPG-ONE PIECE⚔️ 〙`
+}
+
+exports.help = help
+
